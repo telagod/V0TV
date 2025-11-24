eb35c74904650f7ca51609d8dcb082adb509727e
telagod <telagod@users.noreply.github.com>
2025-11-24 22:49:53 +0800

    chore: format code

--- src/app/play/components/VideoPlayer/AdFilterLoader.ts
+++ src/app/play/components/VideoPlayer/AdFilterLoader.ts
@@ -124,7 +124,9 @@ export function createAdFilterLoader() {
     private loader: InstanceType<typeof DefaultLoader>;
 
     constructor(config: unknown) {
-      this.loader = new DefaultLoader(config as Parameters<typeof DefaultLoader>[0]);
+      this.loader = new DefaultLoader(
+        config as Parameters<typeof DefaultLoader>[0]
+      );
     }
 
     destroy() {
